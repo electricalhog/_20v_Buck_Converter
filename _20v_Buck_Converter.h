585bef169196247bb3caaf16fe79ee46f6b960c2
Daniel <electricalhog10@gmail.com>
2023-01-12 22:29:59 -0500

    Parameter tuning

--- _20v_Buck_Converter.h
+++ _20v_Buck_Converter.h
@@ -15,7 +15,7 @@
 
 
 //define constants
-#define FREQUENCY 250 //frequency in kHz of the switching
+#define FREQUENCY 420 //frequency in kHz of the switching
 #define MAX_DUTY (120000/FREQUENCY) //what the PER reg should be set to for proper frequency
 #define CELL_CUTOUT_VOLTAGE 3.5 //the buck convert will shut down output if estimated cell voltage drops to this level or lower; starts at 3.5
 #define SET_VOLTAGE 20.0 //voltage out
@@ -25,7 +25,7 @@
 #define NUM_CELLS 8 //the number of series lipo cells on the input
 #define VOLTAGE_SCALE 118.5 //the scale for the voltage dividers
 #define CURRENT_SCALE 36.5 //the scale for the current measuring
-#define CURRENT_OFFSET 2.2 //the offset for the current measuring
+#define CURRENT_OFFSET 11.7 //the offset for the current measuring
 #define SATURATION_CURRENT 45 //amps considered to saturate the inductor
 #define SOFT_START_RATE 1250 //high values give slower soft start
 #define AMP_SMOOTHING 10 //higher values average more
